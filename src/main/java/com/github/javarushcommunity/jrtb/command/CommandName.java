536f9b12a0c2109b643ac8b41c6a285a0660492b
Yurii <libak556@gmail.com>
2022-03-25 00:04:16 +0200

    Step_5_TBJ-1: added repository layer.

--- src/main/java/com/github/javarushcommunity/jrtb/command/CommandName.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/CommandName.java
@@ -8,7 +8,8 @@ public enum CommandName {
     START("/start"),
     STOP("/stop"),
     NO("/no"),
-    HELP("/help");
+    HELP("/help"),
+    STAT("/stat");
 
     private final String commandName;
 
